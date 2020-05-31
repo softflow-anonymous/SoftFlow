a813099f9965b19b64266333537160bca06b1cc9
softflow-anonymous <softflowcode@gmail.com>
2020-05-31 14:57:12 +0900

    initialized

---
+++ pointclouds/scripts/train.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+cates="airplane"
+for (( i = 1000; i <= 15000; i=i+1000)); do
+    python train.py \
+        --distributed \
+        --cates ${cates} \
+        --epoch ${i} \
+        --batch_size 128 \
+        --n_flow 12 \
+        --multi_freq 4 \
+        --n_flow_AF 9 \
+        --h_dims_AF 256-256-256
+    echo "Done"
+done
